da7fe1129fc661a6fbaf5aa123d46eb077f00b53
Michiel Peters <michiel.peters@avanade.com>
2020-07-21 12:25:26 +0200

    Updates

---
+++ precache-manifest.23a1421052a0eb83f2333c44a622b9df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110133657ae1b950cf8a61c7f77635da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe1c558133d610b2985f",
+    "url": "/static/css/main.bb6a6371.chunk.css"
+  },
+  {
+    "revision": "c58a95f7349c04e6719f",
+    "url": "/static/js/2.fa93647a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa93647a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1c558133d610b2985f",
+    "url": "/static/js/main.e70e84d5.chunk.js"
+  },
+  {
+    "revision": "dda71740b1a5abb8533e",
+    "url": "/static/js/runtime-main.32069510.js"
+  }
+]);
