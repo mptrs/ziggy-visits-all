0a736d4c079c504173f41fbb71277e97f6388340
BibiNavasGarcia <bibigarcia@Bibis-MacBook-Pro.local>
2020-07-22 23:07:54 +0200

    added keyboard scrolling

--- src/App.js
+++ src/App.js
@@ -1,125 +1,165 @@
-import React from 'react';
-import './App.css';
-import logo from './img/rampageslayer2000.jpeg';
+import React from "react";
+import "./App.css";
+import logo from "./img/rampageslayer2000.jpeg";
+import ArrowKeysReact from "arrow-keys-react";
 
 class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			counter: 0,
-			websites: [],
-		};
-		this.FILE_KEY = 'file.json';
-		this.COUNTER = 'counter';
-	}
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+      websites: [],
+    };
+    this.goToLink = this.goToLink.bind(this);
 
-	componentDidMount() {
-		const storedFile = JSON.parse(localStorage.getItem(this.FILE_KEY));
-		if (!storedFile || !storedFile.websites) return false;
-		this.setState({ websites: storedFile.websites });
+    ArrowKeysReact.config({
+      left: () => {
+        this.previous();
+      },
+      right: () => {
+        this.next();
+      },
+      up: () => {
+        this.goToLink();
+      },
+    });
 
-		const storedCounter = localStorage.getItem('counter');
-		if (!storedCounter || !Number(storedCounter)) return false;
-		this.setState({ counter: Number(localStorage.getItem(this.COUNTER)) });
-	}
+    this.FILE_KEY = "file.json";
+    this.COUNTER = "counter";
+  }
 
-	previous = () => {
-		if (this.state.counter > 0) {
-			this.setState(
-				(prevState, props) => ({
-					counter: prevState.counter - 1,
-				}),
-				this.updateLocalStorageCounter
-			);
-		}
-	};
+  componentDidMount() {
+    const storedFile = JSON.parse(localStorage.getItem(this.FILE_KEY));
+    if (!storedFile || !storedFile.websites) return false;
+    this.setState({ websites: storedFile.websites });
 
-	next = () => {
-		console.log(this.state.counter + 1, this.state.websites.length);
-		if (this.state.counter + 1 < this.state.websites.length) {
-			this.setState(
-				(prevState, props) => ({
-					counter: prevState.counter + 1,
-				}),
-				this.updateLocalStorageCounter
-			);
-		}
-	};
+    const storedCounter = localStorage.getItem("counter");
+    if (!storedCounter || !Number(storedCounter)) return false;
+    this.setState({ counter: Number(localStorage.getItem(this.COUNTER)) });
+  }
 
-	updateLocalStorageCounter = () => {
-		window.localStorage.setItem(this.COUNTER, this.state.counter);
-	};
+  goToLink = () => {
+    this.link.click();
+  };
 
-	handleUpload = (e) => {
-		const reader = new FileReader();
-		reader.onload = this.handleFileRead;
+  previous = () => {
+    if (this.state.counter > 0) {
+      this.setState(
+        (prevState, props) => ({
+          counter: prevState.counter - 1,
+        }),
+        this.updateLocalStorageCounter
+      );
+    }
+  };
 
-		const file = e.target.files[0];
-		reader.readAsText(file);
-	};
+  next = () => {
+    console.log(this.state.counter + 1, this.state.websites.length);
+    if (this.state.counter + 1 < this.state.websites.length) {
+      this.setState(
+        (prevState, props) => ({
+          counter: prevState.counter + 1,
+        }),
+        this.updateLocalStorageCounter
+      );
+    }
+  };
 
-	handleFileRead = (e) => {
-		const save = JSON.parse(e.target.result);
-		if (!save || !save.websites) {
-			alert('Whoeps, er lijkt iets mis met de json file.');
-			return false;
-		}
+  updateLocalStorageCounter = () => {
+    window.localStorage.setItem(this.COUNTER, this.state.counter);
+  };
 
-		window.localStorage.setItem(this.FILE_KEY, JSON.stringify(save));
-		const { websites } = save;
-		this.setState({ websites: websites });
-	};
+  handleUpload = (e) => {
+    const reader = new FileReader();
+    reader.onload = this.handleFileRead;
 
-	render() {
-		const { websites, counter } = this.state;
-		const currentSite = websites[counter];
-		return (
-			<div className="App">
-				{websites.length === 0 ? (
-					<main>
-						<h1>Stay ahead of the competition!</h1>
-						<img src={logo} width="648" height="455" alt="" />
-						<label className="btn">
-							Upload je bestand
-							<input
-								id="input"
-								name="input"
-								type="file"
-								onChange={this.handleUpload}
-							/>
-						</label>
-					</main>
-				) : (
-					<main>
-						<iframe
-							title="wbsite-frame"
-							src={currentSite}
-							id="websiteFrame"
-						></iframe>
-						<div className="actions">
-							<button
-								className="btn"
-								disabled={counter === 0}
-								onClick={this.previous}
-							>
-								Vorige
-							</button>
-							<a href={currentSite} target="_blank" rel="noopener noreferrer">
-								<h3>Bezoek {currentSite}</h3>
-							</a>
-							<button
-								className="btn"
-								disabled={counter + 1 === websites.length}
-								onClick={this.next}
-							>
-								Volgende
-							</button>
-						</div>
-					</main>
-				)}
-			</div>
-		);
-	}
+    const file = e.target.files[0];
+    reader.readAsText(file);
+  };
+
+  handleFileRead = (e) => {
+    const save = JSON.parse(e.target.result);
+    if (!save || !save.websites) {
+      alert("Whoeps, er lijkt iets mis met de json file.");
+      return false;
+    }
+
+    window.localStorage.setItem(this.FILE_KEY, JSON.stringify(save));
+    const { websites } = save;
+    this.setState({ websites: websites });
+  };
+
+  render() {
+    const { websites, counter } = this.state;
+    const currentSite = websites[counter];
+
+    return (
+      <div className="App">
+        {websites.length === 0 ? (
+          <main>
+            <h1>Stay ahead of the competition!</h1>
+            <img src={logo} width="648" height="455" alt="" />
+            <label className="btn">
+              Upload je bestand
+              <input
+                id="input"
+                name="input"
+                type="file"
+                onChange={this.handleUpload}
+              />
+            </label>
+          </main>
+        ) : (
+          <main>
+            <iframe
+              title="website-frame"
+              src={currentSite}
+              id="websiteFrame"
+            ></iframe>
+            <div className="actions">
+              <button
+                {...ArrowKeysReact.events}
+                className="btn"
+                disabled={counter === 0}
+                onClick={this.previous}
+              >
+                Vorige
+              </button>
+              <button
+                {...ArrowKeysReact.events}
+                id="btn"
+                type="button"
+                onClick={this.goToLink.bind(this)}
+                ref={(ref) => {
+                  this.bt = ref;
+                }}
+              >
+                <a
+                  id="link"
+                  href={currentSite}
+                  ref={(ref) => {
+                    this.link = ref;
+                  }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <h3>Bezoek {currentSite}</h3>
+                </a>
+              </button>
+              <button
+                {...ArrowKeysReact.events}
+                className="btn"
+                disabled={counter + 1 === websites.length}
+                onClick={this.next}
+              >
+                Volgende
+              </button>
+            </div>
+          </main>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
